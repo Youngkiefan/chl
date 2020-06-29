5f832fb9e7b04c7763f6bfcd2ad52a713010f06b
Youngkiefan <787750286@qq.com>
2020-06-29 15:12:39 +0800

    第二节课

--- demo1.py
+++ demo1.py
@@ -50,14 +50,18 @@ print(name)
 # print("两段内容长度的和：",len(a)+len(b))  
 
 
+
+
+# 元组基础
+
 #元组,下标：从0开始编号
 a = (1,2,3,4,"哈哈哈哈","llalalal",True,False,"dasdaf",32423)
 
 
 #切片
-print(a[6])
-print(a[0:4])#左闭右开（区间）
-print(a[9:])
+# print(a[6])
+# print(a[0:4])#左闭右开（区间）
+# print(a[9:])
 
 #查找下标,如果有很多个下标的时候，那么会从左往右输出最近的一个
 # print(a.index("llalalal"))
@@ -67,3 +71,130 @@ print(a[9:])
 # #二维元组
 # b = (a,"丽丽","楷楷")
 # print(b[0][4])
+
+
+
+
+
+
+# # 数组基础
+
+# # 元组一旦写好后就不能修改，但是数组可以
+# a = [1,2,3,4,"哈哈哈哈","llalalal",True,False,"dasdaf",32423]
+# # a.append("ajklf")
+# print(a)
+
+# # 插入一个值
+# a.insert(1,"fasdf")
+# print(a)
+# # a.index()
+# a.count()
+
+# # a.pop():类似于剪切的功能，
+# b = a.pop(1)
+# c = a.pop(2)
+# print(b+c)
+
+
+# a.clear():清空数组
+# a.clear()
+# print(a)
+
+# # a.extend
+# r = ["affadf",2432]
+# a.extend(a)
+# # print(a)
+
+
+
+# a.remove 移走
+# a.remove("哈哈哈哈")
+# print(a)
+# remove(1)=remove(True)
+# remove(0)=remove(False)
+
+# d = [1,0,True,False]
+# e = d.count(d)
+# d.remove(0)
+# print(d)
+
+
+
+# 下标超出范围=越界
+# xx = [2,23,4,5,3]
+
+
+
+"""
+python的语法：
+1、所有方法都是小括号结尾，比如 print(),len(),input()
+2、所有元组、数组、字典取值都是用[],比如：a[3]
+3、元组、数组、字典的定义分别用的是()[]{}
+"""
+
+# 字典的基础
+
+"""
+字典的特点：
+1、字典中的值是没有顺序的
+2、字典的结构必须是键值对的结构:key:value
+3、字典的取值是通过key去取value
+"""
+# a = {"name":"张三",
+#     "age":25,
+#     "phone":"tvwgbh"
+# }
+
+
+
+# # 取值
+# print(a["name"])
+# # 新增
+# a["high"] = "170cm"
+# print(a)
+# # 修改
+# a["name"]="youngkiefan"
+# print(a)
+
+#常用的方法
+# get:相当于取值
+# b = a.get("name")
+# print(b)
+# # pop：
+# b = a.pop("name")
+# print(a)
+# # update ：相当于修改
+# a.update(name="zhangsan")
+# print(a)
+
+
+# 两个的区别
+# print(a["name"])
+# print(a.get("name"))
+# 错了前者是none后者是报错
+
+
+
+
+
+# # 数组和字典的通用删除
+# del a["name"]
+# print(a)
+
+# del a[2]
+
+
+"""
+练习：获取用户输入的个人信息并且存储到字典中
+个人信息包括姓名、年龄、性别
+"""
+
+a = input("请输入姓名：")
+b = input("请输入年龄：")
+c = input("请输入性别：")
+d = {"name":a,
+     "age":b,
+     "sex":c
+     }
+
+print(d)
